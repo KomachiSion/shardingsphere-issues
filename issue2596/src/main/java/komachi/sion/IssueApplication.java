ac645988838e82c438ddbcf88017ab687b2b000d
KomachiSion <263976490@qq.com>
2019-06-24 09:57:32 +0800

    add issue#2596

---
+++ issue2596/src/main/java/komachi/sion/IssueApplication.java
@@ -0,0 +1,26 @@
+package komachi.sion;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * fixed in 4.0.0-RC2
+ *
+ * @author yangyi
+ */
+@SpringBootApplication
+public class IssueApplication {
+    
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(IssueApplication.class, args)) {
+            process(applicationContext);
+        }
+    }
+    
+    private static void process(ConfigurableApplicationContext applicationContext) {
+        DataSource dataSource = applicationContext.getBean(DataSource.class);
+    }
+}
